8404bc1c2982363cc057f8205a74809762b71dc5
pradipgyawali234 <147186517+pradipgyawali234@users.noreply.github.com>
2023-10-06 23:08:15 +0545

    refactor rename

--- src/BankDetails.java
+++ src/BankDetails.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 public class BankDetails {
 
-    private Long acc_no;
-    private String acc_name;
-    private String acc_type;
+    private Long accountNumber;
+    private String accountName;
+    private String accountType;
     private long balance;
     Scanner sc = new Scanner(System.in);
 
@@ -17,21 +17,21 @@ public class BankDetails {
         try {
 
             System.out.println("Enter your Account No.:");
-            acc_no = sc.nextLong();
+            accountNumber = sc.nextLong();
 
             System.out.println("Enter your Account Type:");
-            acc_type = sc.next();
+            accountType = sc.next();
             System.out.println("Enter your Account Name:");
-            acc_name = sc.next();
+            accountName = sc.next();
             System.out.println("Enter your Account Balance:");
             balance = sc.nextLong();
             String sql = "insert into table1(acc_no,acc_type,name,balance) values (?,?,?,?)";
             PreparedStatement statement = connection.prepareStatement(sql);
 
 
-            statement.setLong(1, acc_no);
-            statement.setString(2, acc_type);
-            statement.setString(3, acc_name);
+            statement.setLong(1, accountNumber);
+            statement.setString(2, accountType);
+            statement.setString(3, accountName);
             statement.setLong(4, balance);
             statement.executeUpdate();
             System.out.println("Record updated.");
@@ -48,17 +48,17 @@ public class BankDetails {
             Statement statement = connection.createStatement();
 
             System.out.println("Enter the account number you want to search.");
-            acc_no = sc.nextLong();
+            accountNumber = sc.nextLong();
 
-            ResultSet resultSet = statement.executeQuery("select * from table1 where acc_no =" + acc_no);
+            ResultSet resultSet = statement.executeQuery("select * from table1 where acc_no =" + accountNumber);
             while (resultSet.next()) {
 
-                acc_no = resultSet.getLong(1);
-                System.out.println("Account Number:" + acc_no);
-                acc_type = resultSet.getString(2);
-                System.out.println("Account Type:" + acc_type);
-                acc_name = resultSet.getString(3);
-                System.out.println("Account Name:" + acc_name);
+                accountNumber = resultSet.getLong(1);
+                System.out.println("Account Number:" + accountNumber);
+                accountType = resultSet.getString(2);
+                System.out.println("Account Type:" + accountType);
+                accountName = resultSet.getString(3);
+                System.out.println("Account Name:" + accountName);
                 balance = resultSet.getLong(4);
                 System.out.println("Account Balance:" + balance);
             }
@@ -74,7 +74,7 @@ public class BankDetails {
         long amt;
 
         System.out.print("Enter Account no. : ");
-        acc_no = sc.nextLong();
+        accountNumber = sc.nextLong();
         System.out.println("Enter the amount you want to deposit:");
         amt = sc.nextLong();
         try {
@@ -82,14 +82,14 @@ public class BankDetails {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bank", "root", "");
 
 
-            String query = "select * from table1 where acc_no=" + acc_no;
+            String query = "select * from table1 where acc_no=" + accountNumber;
             Statement statement1 = connection.createStatement();
             ResultSet resultSet = statement1.executeQuery(query);
             while (resultSet.next()) {
                 balance = resultSet.getLong("balance");
                 long deposited = balance + amt;
 
-                String sql = "UPDATE table1 SET balance = ? WHERE acc_no = " + acc_no;
+                String sql = "UPDATE table1 SET balance = ? WHERE acc_no = " + accountNumber;
                 PreparedStatement statement = connection.prepareStatement(sql);
 
 
@@ -108,14 +108,14 @@ public class BankDetails {
     public void withdrawal() {
         long amt;
         System.out.print("Enter Account No : ");
-        acc_no = sc.nextLong();
+        accountNumber = sc.nextLong();
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bank", "root", "");
 
 
-            String query = "select * from table1 where acc_no=" + acc_no;
+            String query = "select * from table1 where acc_no=" + accountNumber;
             Statement statement1 = connection.createStatement();
             ResultSet resultSet = statement1.executeQuery(query);
             while (resultSet.next()) {
@@ -127,7 +127,7 @@ public class BankDetails {
                     System.out.println("After withdrawal, you have: " + balance + "on your account");
 
 
-                    String sql = "UPDATE table1 SET balance = ? WHERE acc_no = " + acc_no;
+                    String sql = "UPDATE table1 SET balance = ? WHERE acc_no = " + accountNumber;
                     PreparedStatement statement = connection.prepareStatement(sql);
 
 
